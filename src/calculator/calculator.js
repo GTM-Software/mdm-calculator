df90e292de24a41644caeb10e0bbd4c8f0e30170
Ankur Gupta <ankurdotcom@gmail.com>
2023-10-09 17:00:20 +0530

    Inital Code Setup Done

---
+++ src/calculator/calculator.js
@@ -0,0 +1,18 @@
+"use strict"
+
+// function greet(user){
+//     console.log(`Welcome ${user} MDMCalcualtor Started`);
+// }
+
+// module.exports = {greeterFn : greet};
+
+
+const greet = (user) => console.log(`Welcome ${user} MDMCalcualtor Started`);
+const greet2 = (user) => console.log(`Welcome ${user} MDMCalcualtor Started`);
+
+
+
+
+
+module.exports = {greet, greet2};
+
---
+++ src/db/db.js
@@ -0,0 +1,39 @@
+"use strict"
+
+const MENU_MASTER = [{
+
+    "DAY" : "SUN",
+    "MENU" : "KHICHADI",
+    "COST" : "2",
+},{
+
+    "DAY" : "MON",
+    "MENU" : "DAL CHAWAL",
+    "COST" : "3",
+},{
+
+    "DAY" : "TUE",
+    "MENU" : "DAL CHAWAL",
+    "COST" : "4",
+},
+{
+
+    "DAY" : "WED",
+    "MENU" : "DAL CHAWAL",
+    "COST" : "2",
+},{
+
+    "DAY" : "THU",
+    "MENU" : "DALIA",
+    "COST" : "3",
+},{
+
+    "DAY" : "FRI",
+    "MENU" : "DAL CHAWAL",
+    "COST" : "4",
+},{
+
+    "DAY" : "SAT",
+    "MENU" : "PURI SABJI",
+    "COST" : "5",
+},];
